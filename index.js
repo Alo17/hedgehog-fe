515cf985b5d7d24fd221c444d988025bfc746619
Alo17 <alondrahr017@gmail.com>
2018-12-02 15:44:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var form = document.querySelector('form');
 var button = document.querySelector('.submit-btn')
 var spinner = document.querySelector('.spinner')
 var inputField = document.querySelector('#input-field');
-var hedgie1 = document.querySelector('#hedgie1');
-var hedgie2 = document.querySelector('#hedgie2');
-var hedgie3 = document.querySelector('#hedgie3');
-var hedgie4 = document.querySelector('#hedgie4');
+var hedgie1 = document.querySelector('#ocean1');
+var hedgie2 = document.querySelector('#ocean2');
+var hedgie3 = document.querySelector('#ocean3');
+var hedgie4 = document.querySelector('#ocean4');
 
 button.addEventListener("click", function(event) {
   event.preventDefault();
@@ -16,7 +16,7 @@ button.addEventListener("click", function(event) {
 function getImages(keyword) {
   button.value = "Looking for photos..."
 
-  fetch(`http://localhost:3000/hedgie/${keyword}`)
+  fetch(`http://localhost:3000/Ocean/${keyword}`)
     .then(response => response.json())
     .then(images => updateImages(images))
     .then(() => button.value = "Get Hedgie Photos")
@@ -29,7 +29,6 @@ function clearInput() {
 }
 
 function updateImages(images) {
-  console.log(images);
   hedgie1.src = images[0];
   hedgie2.src = images[1];
   hedgie3.src = images[2];
